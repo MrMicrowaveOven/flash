d1776eb07a1dcbea7510552ed64e3feb71b4d534
Benjamin Zagorski <benj@zagorski.com>
2019-04-25 11:47:29 -0700

    Now responds to sender

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,23 +1,25 @@
 class SmsController < ApplicationController
   def create
     p params
-    # from = params["from"]
+    from = params["from"]
     require 'rubygems'
     require 'twilio-ruby'
 
     # Your Account Sid and Auth Token from twilio.com/console
     # DANGER! This is insecure. See http://twil.io/secure
-    account_sid = ENV['TWILIO_SID']
-    auth_token = ENV['TWILIO_TOKEN']
-    @client = Twilio::REST::Client.new(account_sid, auth_token)
+    if from
+      account_sid = ENV['TWILIO_SID']
+      auth_token = ENV['TWILIO_TOKEN']
+      @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-    message = @client.messages.create(
-                                 from: '+14152124906',
-                                 body: 'Hello Nick!',
-                                 to: '+17148099426'
-                               )
+      message = @client.messages.create(
+                                   from: '+14152124906',
+                                   body: 'Hello Nick!',
+                                   to: from
+                                 )
 
-    puts message.sid
+      puts message.sid
+    end
     render json: params
   end
 end
