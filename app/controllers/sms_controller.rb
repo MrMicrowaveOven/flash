ab13442e66f219e336db38854f411983a16f03f7
Benjamin Zagorski <benj@zagorski.com>
2019-05-19 10:49:13 -0700

    Added dumb response tags

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -42,7 +42,7 @@ class SmsController < ApplicationController
       end
       puts message.sid
     end
-    render json: response, content_type: 'application/xml'
+    render json: '<Response>' + response + '</Response>', content_type: 'application/xml'
   end
   def send_intro_text
     @client.messages.create(
