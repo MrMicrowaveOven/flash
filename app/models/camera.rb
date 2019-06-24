ad9ced2a0d6283cc1644617871c62bea6e0f7d7a
Benjamin Zagorski <benj@zagorski.com>
2019-06-24 11:08:59 -0700

    Added contact_number and active columns to cameras, changed to 15 minute camera test system

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -15,13 +15,41 @@ class Camera < ApplicationRecord
       p "Testing Camera ##{camera.id}"
       response = Net::HTTP.get(camera.tunnel_url, '/')
       p "Camera ##{camera.id}"
-      p response
-      p @client.messages.create(
-        from: '+14152124906',
-        to: '+17148099426',
-        body: "Camera ##{camera.id}: #{response.slice(0, 10)}"
-      )
-      # sleep 10
+      if response.index('https://s3') == 0
+        if !camera.active
+          camera.update(active: true)
+          p @client.messages.create(
+            from: '+14152124906',
+            to: camera.contact_number,
+            body: "Camera ##{camera.id} has just become active."
+          )
+        else
+          p "Camera ##{camera.id} is still active"
+        end
+        p response
+        p @client.messages.create(
+          from: '+14152124906',
+          to: '+17148099426',
+          body: "Camera ##{camera.id} is working.}"
+        )
+      else
+        if camera.active
+          camera.update(active: false)
+          p @client.messages.create(
+            from: '+14152124906',
+            to: camera.contact_number,
+            body: "Camera ##{camera.id} has just become inactive."
+          )
+        else
+          p "Camera ##{camera.id} is still inactive"
+        end
+        p response
+        p @client.messages.create(
+          from: '+14152124906',
+          to: '+17148099426',
+          body: "Camera ##{camera.id}: #{response.slice(0, 10)}"
+        )
+      end
     end
   end
 end
