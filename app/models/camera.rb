cc2f52798d81a9660b8d557d71ad0be9ae545a33
Benjamin Zagorski <benj@zagorski.com>
2019-06-26 17:00:16 -0700

    camera_test refactor

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,44 +1,78 @@
 class Camera < ApplicationRecord
   has_many :pictures
   default_scope { order(:created_at) }
+
+  require 'rubygems'
+  require 'twilio-ruby'
+
+  require 'net/http'
+
+  account_sid = ENV['TWILIO_SID']
+  auth_token = ENV['TWILIO_TOKEN']
+  CLIENT = Twilio::REST::Client.new(account_sid, auth_token)
+
   def self.camera_test
     p 'Beginning camera test'
-    require 'rubygems'
-    require 'twilio-ruby'
-
-    require 'net/http'
-
-    account_sid = ENV['TWILIO_SID']
-    auth_token = ENV['TWILIO_TOKEN']
-    @client = Twilio::REST::Client.new(account_sid, auth_token)
 
     Camera.find_each do |camera|
       p "Testing Camera ##{camera.id}"
-      response = Net::HTTP.get(camera.tunnel_url, '/')
+      begin
+        response = camera.test_camera
+      rescue
+        camera.set_as_inactive_and_report
+      end
       p "Camera ##{camera.id}"
       if response.index('https://s3') == 0
-        if !camera.active
-          camera.update(active: true)
-          p @client.messages.create(
-            from: '+14152124906',
-            to: camera.contact_number,
-            body: "Camera ##{camera.id} has just become active."
-          )
-        else
-          p "Camera ##{camera.id} is still active"
-        end
+        camera.active_procedure
       else
-        if camera.active
-          camera.update(active: false)
-          p @client.messages.create(
-            from: '+14152124906',
-            to: camera.contact_number,
-            body: "Camera ##{camera.id} has just become inactive."
-          )
-        else
-          p "Camera ##{camera.id} is still inactive"
-        end
+        camera.inactive_procedure
       end
     end
+  end
+
+  def active_procedure
+    if !active
+      set_as_active_and_report
+    else
+      p "Camera ##{id} is still active"
+    end
+  end
+
+  def inactive_procedure
+    if active
+      set_as_inactive_and_report
+    else
+      p "Camera ##{id} is still inactive"
+    end
+  end
+
+  def set_as_active_and_report
+    update(active: true)
+    report_active
+  end
+
+  def report_inactive
+    p CLIENT.messages.create(
+      from: '+14152124906',
+      to: contact_number,
+      body: "Camera ##{id} has just become inactive."
+    )
+  end
+
+  def report_active
+    p CLIENT.messages.create(
+      from: '+14152124906',
+      to: contact_number,
+      body: "Camera ##{id} has just become active."
+    )
+  end
+
+  def set_as_inactive_and_report
+    update(active: false)
+    report_inactive
+  end
+
+  def test_camera
+    Net::HTTP.get(tunnel_url, '/')
   end
 end
