a5320ad40cf3da5278a5f611ed6e8f941186b4c6
mrmicrowaveoven <benj@zagorski.com>
2020-02-09 17:46:42 -0800

    Optimized picture.id

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -2,6 +2,6 @@ class CamerasController < ApplicationController
   def show
     camera = Camera.find(params[:id])
     picture = camera.pictures.where(sent_to_user: false).first
-    render json: {code: 200, picture_id: picture ? picture.id : nil}
+    render json: { code: 200, picture_id: picture&.id }
   end
 end
