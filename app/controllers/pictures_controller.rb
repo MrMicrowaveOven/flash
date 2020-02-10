8eac20425e7882b828f03ddad3fc8aa0c7514a87
mrmicrowaveoven <benj@zagorski.com>
2020-02-09 19:15:05 -0800

    photo_url now gets updated

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,7 @@
 class PicturesController < ApplicationController
   def update
     picture = Picture.find_by_id(params[:id])
-    picture.update(photo_url: url)
+    picture.update(photo_url: params['photo_url'])
     picture.send_to_user
   end
 end
