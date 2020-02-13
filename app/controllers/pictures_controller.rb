a7216523f93b8387c0eb067a7140304810b8fc8f
mrmicrowaveoven <benj@zagorski.com>
2020-02-12 17:00:38 -0800

    Actual fix

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,6 +3,6 @@ class PicturesController < ApplicationController
     picture = Picture.find_by_id(params[:id])
     picture.update(photo_url: params['photo_url'])
     picture.send_to_user
-    render :json { code: 200, picture_id: picture&.id }
+    render json: { code: 200, picture_id: picture&.id }
   end
 end
