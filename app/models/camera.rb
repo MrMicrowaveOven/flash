4738331c37d82b4963ee1d9f343712d754bee6a0
mrmicrowaveoven <benj@zagorski.com>
2020-02-13 11:48:00 -0800

    Added annotate gem

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: cameras
+#
+#  id             :bigint           not null, primary key
+#  phone_number   :string
+#  tunnel_url     :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  contact_number :string
+#  active         :boolean
+#
+
 class Camera < ApplicationRecord
   has_many :pictures
   default_scope { order(:created_at) }
--- test/models/camera_test.rb
+++ test/models/camera_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: cameras
+#
+#  id             :bigint           not null, primary key
+#  phone_number   :string
+#  tunnel_url     :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  contact_number :string
+#  active         :boolean
+#
+
 require 'test_helper'
 
 class CameraTest < ActiveSupport::TestCase
