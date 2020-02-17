5f52f46a4d6ee7c72fca2cd31e30c20bfca630d4
mrmicrowaveoven <benj@zagorski.com>
2020-02-17 15:41:56 -0800

    Remove logs

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -1,10 +1,6 @@
 class CamerasController < ApplicationController
   def index
-    p params
-    p params[:mac_address]
-    p params['mac_address']
     camera = Camera.find_by_mac_address(params['mac_address'])
-    p camera
     camera.touch
     picture = camera.pictures.where(sent_to_user: false).first
     render json: { code: 200, picture_id: picture&.id }
