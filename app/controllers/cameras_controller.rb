a5af9650ffc718f497b602e4559c3bc81b08e7ca
mrmicrowaveoven <benj@zagorski.com>
2020-02-17 14:01:22 -0800

    Swapped from id to mac address

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -1,6 +1,6 @@
 class CamerasController < ApplicationController
   def show
-    camera = Camera.find(params[:id])
+    camera = Camera.find_by_mac_address(params[:mac_address])
     camera.touch
     picture = camera.pictures.where(sent_to_user: false).first
     render json: { code: 200, picture_id: picture&.id }
