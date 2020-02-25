03e46cb4756505890eaac94f40fe995a176560b1
mrmicrowaveoven <benj@zagorski.com>
2020-02-24 22:40:41 -0800

    Params now don't raise a 500

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -4,15 +4,15 @@ class CamerasController < ApplicationController
     if camera
       camera.touch
       picture = camera.pictures.where(sent_to_user: false).first
-      if params['test'] == true
-        render json: { code: 200, picture_id: None, mac_address: true}
+      if params['test']
+        render json: { code: 200, picture_id: nil, mac_address: true}
       else
         render json: { code: 200, picture_id: picture&.id }
       end
     else
       UnknownMacAddress.inform(params['mac_address'])
-      if params['test'] == true
-        render json: { code: 200, picture_id: None, mac_address: false}
+      if params['test']
+        render json: { code: 200, picture_id: nil, mac_address: false}
       end
     end
   end
